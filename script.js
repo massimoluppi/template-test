8cbf59650236b6bb8e8a8d7fe9d24f19bcfcbcb1
Massimo Luppi <massimo.luppi@yahoo.com>
2022-03-04 18:44:22 +0100

    New version
    New version

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-console.log("ancora DP MP");
+// Get Quotes from API
+
+let apiQuotes = [];
+
+async function getQuotes() {
+	const apiUrl = "https://type.fit/api/quotes";
+	try {
+		const response = await fetch(apiUrl);
+		apiQuotes = await response.json();
+		const fullQuote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+		document.getElementById("quote").innerText = fullQuote.text;
+		document.getElementById("author").innerText = fullQuote.author;
+	} catch (error) {
+		// Catch Error here
+	}
+}
+
+// On Load
+let newQuote = document.getElementById("new-quote");
+newQuote.addEventListener("click", (Event) => getQuotes());
